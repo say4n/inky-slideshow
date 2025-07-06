3d4a1a1fa1901fde2aa2ccd835a07d71b74bf835
Sayan Goswami <goswami.sayan47@gmail.com>
2025-07-06 13:29:39 +0100

    adds logging

--- src/inky_slideshow/slideshow.py
+++ src/inky_slideshow/slideshow.py
@@ -1,7 +1,9 @@
 from glob import glob
+import random
 import time
 import click
 from inky.auto import auto
+from loguru import logger
 from PIL import Image
 
 
@@ -18,16 +20,25 @@ def main(path):
     for ext in allowed_extensions:
         images.extend(glob(f"{path}/*{ext}"))
 
-    index = 0
+    if not images:
+        logger.error("No images found in the specified directory.")
+        exit(1)
+    else:
+        logger.info(f"Found {len(images)} images in the directory: {path}")
+
+    index = random.randint(0, len(images) - 1)
 
     while True:
         current_image = images[index % len(images)]
+        logger.info(f"Displaying image: {current_image} (Index: {index})")
+
         image = Image.open(current_image).convert("RGB")
         image = image.resize(inky_display.resolution)
         inky_display.set_image(image)
         inky_display.show()
         index += 1
 
+        logger.info("Waiting for 60 seconds before displaying the next image...")
         time.sleep(60)
 
 
